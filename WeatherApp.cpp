8e3667508a23d7e80adb023871a754fc3f097028
TopherBriggs <christophertbriggs@gmail.com>
2022-08-10 02:43:30 -0400

    can now retrieve tempature from a hardcoded station

--- WeatherApp.cpp
+++ WeatherApp.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include <curl/curl.h>
 #include <string>
+#include <json/json.h>
 
+static int celsiusToFahrenheit(int temp)
+{
+    return round(temp * 1.8 + 32);
+}
 
 //function that is called when curl when retreiving contents which turns the output to a string
 static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp)
@@ -14,15 +19,17 @@ static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* use
 int main() {
     CURL* curl;
     CURLcode res;
-
+    Json::Value root;
+    Json::Reader reader;
     std::string apiResponse;
+    
 
     curl_global_init(CURL_GLOBAL_ALL);
-
+    
     curl = curl_easy_init();
     if (curl) {
 
-        curl_easy_setopt(curl, CURLOPT_URL, "https://api.weather.gov/stations/KARB/observations/latest");
+        curl_easy_setopt(curl, CURLOPT_URL, "https://api.weather.gov/stations/KDET/observations/latest");
         curl_easy_setopt(curl, CURLOPT_USERAGENT, "Mr. Weatherman");
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &apiResponse);
@@ -32,8 +39,17 @@ int main() {
         }
         curl_easy_cleanup(curl);
 
-        std::cout << "data:\n" << apiResponse << std::endl;
+        //std::cout << "data:\n" << apiResponse << std::endl;
+        if (!(reader.parse(apiResponse, root)))
+        {
+            std::cout << "Error parsing the string" << std::endl;
+        }
+        //std::cout << root << std::endl;
+        int temperature = root["properties"]["temperature"]["value"].asInt();
+       
+        std::cout << "Tempature in Detroit: " << celsiusToFahrenheit(temperature) << "F" << std::endl;
     }
+    
     curl_global_cleanup();
     return 0;
 }
